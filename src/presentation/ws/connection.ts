82a36d12f03a2d35a411bbe455e51199db41073e
JoonmoYeon <yunedoopea610@gmail.com>
2019-04-15 22:18:34 +0900

    Remove unnecessary logging code

--- src/presentation/ws/connection.ts
+++ src/presentation/ws/connection.ts
@@ -1,7 +1,5 @@
 import { Socket } from 'socket.io';
 
-const listener = (socket: Socket) => {
-  console.log('a user connected', socket.id);
-};
+const listener = (socket: Socket) => {};
 
 export default listener;
