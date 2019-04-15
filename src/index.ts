bb836c042530b39f9a59166ff324c1d8db75cd59
JoonmoYeon <yunedoopea610@gmail.com>
2019-04-15 09:16:33 +0900

    Fix vague logging text

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const server = app();
 const port = Number(process.env.PORT) || 8080;
 
 server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+  console.log(`listening on port:${port}`);
 });
