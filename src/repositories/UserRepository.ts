43718b379392689d3b12fe4881f642464d2a54e0
Joonmo-dev <yunedoopea610@gmail.com>
2019-04-26 11:08:57 +0900

    Add implementation of userRepository

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,23 @@
+import { Db, Collection, InsertOneWriteOpResult } from 'mongodb';
+import { UserRepository } from '../services/repository/User';
+import User from '../entities/User';
+
+export class InmemoryUserRepository implements UserRepository {
+  public readonly collection: Collection;
+  constructor(db: Db, collectionName: string) {
+    this.collection = db.collection(collectionName);
+  }
+
+  async create(user: User): Promise<boolean> {
+    const result: InsertOneWriteOpResult = await this.collection.insertOne(
+      user,
+    );
+    return !!result.result.ok;
+  }
+  async findByUsername(username: string): Promise<User | null> {
+    return await this.collection.findOne({ username });
+  }
+  async isValid(user: User, password: string): Promise<boolean> {
+    return user.password === password;
+  }
+}
