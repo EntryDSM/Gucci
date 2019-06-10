712f17ec2ed5766267e309216c4be26057f82c1a
Joonmo-dev <yunedoopea610@gmail.com>
2019-06-10 11:30:04 +0900

    Add chat routes

---
+++ src/presentation/http/chat.ts
@@ -0,0 +1,100 @@
+import { Router } from 'express';
+import { Db } from 'mongodb';
+import jsonwebtoken from 'jsonwebtoken';
+import { jwtSecretKey } from '../../config';
+import { HttpError } from './error';
+import { ChatMapper } from '../../database/mapper/chat';
+import { AuthMapper } from '../../database/mapper/auth';
+import { User } from '../../entities/user';
+
+export class Chat {
+  private router: Router;
+  private authMapper: AuthMapper;
+  private chatMapper: ChatMapper;
+
+  constructor(private db: Db) {
+    this.router = Router();
+    this.authMapper = new AuthMapper(db);
+    this.chatMapper = new ChatMapper(db);
+
+    this.router.use(async (req, res, next) => {
+      const token = req.headers.authorization;
+
+      if (token) {
+        try {
+          const decoded = jsonwebtoken.verify(token, jwtSecretKey);
+
+          const requestedUser = <User>decoded;
+          const user = await this.authMapper.findByEmail(requestedUser.email);
+
+          if (user) {
+            next();
+          } else {
+            throw Error();
+          }
+        } catch (error) {
+          next(new HttpError(401, 'invalid token'));
+        }
+      } else {
+        next(new HttpError(401, 'unauthorized'));
+      }
+    });
+
+    this.router.get('/messages', async (req, res, next) => {
+      const {
+        room,
+        startMillisecond: startMillisecondStr,
+        limit: limitStr,
+      }: {
+        room: string;
+        startMillisecond?: string;
+        limit?: string;
+      } = req.query;
+
+      try {
+        const startMillisecond = startMillisecondStr
+          ? parseInt(startMillisecondStr, 10)
+          : Date.now();
+        const limit = limitStr ? parseInt(limitStr, 10) : 1;
+
+        const messages = await this.chatMapper.findMessagesByRoom(
+          room,
+          startMillisecond,
+          limit,
+        );
+        res.json(messages);
+      } catch (error) {
+        next(new HttpError(400, 'bad request'));
+      }
+    });
+
+    this.router.get('/message-threads', async (req, res, next) => {
+      const {
+        startMillisecond: startMillisecondStr,
+        limit: limitStr,
+      }: {
+        startMillisecond?: string;
+        limit?: string;
+      } = req.query;
+
+      try {
+        const startMillisecond = startMillisecondStr
+          ? parseInt(startMillisecondStr, 10)
+          : Date.now();
+        const limit = limitStr ? parseInt(limitStr, 10) : 1;
+
+        const messageThreads = await this.chatMapper.findRooms(
+          startMillisecond,
+          limit,
+        );
+        res.json(messageThreads);
+      } catch (error) {
+        next(new HttpError(400, 'bad request'));
+      }
+    });
+  }
+
+  getRouter() {
+    return this.router;
+  }
+}
--- src/presentation/http/index.ts
+++ src/presentation/http/index.ts
@@ -4,13 +4,16 @@ import cors from 'cors';
 import { json } from 'body-parser';
 import { Auth } from './auth';
 import { errorHandler } from './errorHandler';
+import { Chat } from './chat';
 
 export const initHttpRouters = (app: Express, db: Db) => {
   app.use(cors());
   app.use(json());
 
   const auth = new Auth(db);
+  const chat = new Chat(db);
   app.use(auth.getRouter());
+  app.use(chat.getRouter());
 
   app.use(errorHandler);
 };
